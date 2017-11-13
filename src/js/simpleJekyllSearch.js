35586ebdd59c88b0774226ffc62141e59cf79914
Willian Justen <willianjustenqui@gmail.com>
2017-11-13 11:18:40 +0100

    Revert "callback refactor (#32)"
    This reverts commit 87d6c1c91fccf7f99a87c1359681b234288b7cae.

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -21,11 +21,11 @@
                 type: "GET",
                 url: settings.jsonFile,
                 dataType: 'json',
-                success(data, textStatus, jqXHR) {
+                success: function(data, textStatus, jqXHR) {
                     jsonData = data;
                     registerEvent();
                 },
-                error(x, y, z) {
+                error: function(x,y,z) {
                     console.log("***ERROR in simpleJekyllSearch.js***");
                     console.log(x);
                     console.log(y);
