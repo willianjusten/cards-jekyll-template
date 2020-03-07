1c4f11c3324ebfca615baf47ce00abbdf22ca86a
Willian Justen <willianjustenqui@gmail.com>
2020-03-07 13:03:25 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -97,3 +97,6 @@ gulp.task('watch', function () {
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+
+// build to deploy
+gulp.task('build', ['js', 'stylus', 'jekyll-build']);
